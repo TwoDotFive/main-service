790680528292599978ad731770ca52c71773887e
seonmi park <sm000117@naver.com>
2024-07-29 14:01:25 +0900

    feat: User class에서 UserDetails 구현 분리, 회원 구분 id로 하도록 변경

--- src/main/java/com/example/temp/user/domain/User.java
+++ src/main/java/com/example/temp/user/domain/User.java
@@ -6,31 +6,26 @@ import com.example.temp.user.domain.value.Nickname;
 import com.example.temp.user.domain.value.PlatformType;
 import com.example.temp.user.domain.value.UserRole;
 import com.example.temp.user.service.oauth.response.OAuthResponse;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User extends BaseTimeEntity implements UserDetails {
+public class User extends BaseTimeEntity {
     @Id
     private Long id;
     private Long platformId;
+    @Enumerated(EnumType.STRING)
     private PlatformType platformType;
     private String email;
     private String name;
     @Embedded
     private Nickname nickname;
     private String profileImageUrl;
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
     public static User build(OAuthResponse response) {
@@ -56,38 +51,4 @@ public class User extends BaseTimeEntity implements UserDetails {
         this.profileImageUrl = profileImageUrl;
     }
 
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.email;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
 }
--- src/main/java/com/example/temp/user/service/impl/CreateUserServiceImpl.java
+++ src/main/java/com/example/temp/user/service/impl/CreateUserServiceImpl.java
@@ -11,8 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -21,15 +19,11 @@ public class CreateUserServiceImpl implements CreateUserService {
     private final UserRepository userRepository;
     private final JwtTokenService jwtService;
 
-    private static final String ROLE_CLAIM = "role";
-    private static final String NAME_CLAIM = "name";
-    private static final String PROFILE_IMAGE_CLAIM = "profileImageUrl";
-
     @Override
     @Transactional
     public AuthLoginResponse doService(OAuthResponse oAuthResponse) {
         // OAuth 로그인을 시도한 사용자 정보가 DB에 존재하는지 확인 후 없다면 등록
-        User findUser = userRepository.findByEmail(oAuthResponse.getEmail())
+        User findUser = userRepository.findByPlatformId(oAuthResponse.getPlatformId())
                 .orElseGet(() -> {
                     User saveUser = User.build(oAuthResponse);
                     return userRepository.save(saveUser);
@@ -39,7 +33,7 @@ public class CreateUserServiceImpl implements CreateUserService {
         findUser.updateProfile(oAuthResponse.getNickname(), oAuthResponse.getProfileImageUrl());
 
         // JWT Access Token, Refresh Token 발급
-        JwtToken tokens = createJwtToken(findUser);
+        JwtToken tokens = jwtService.createJwtToken(findUser);
         log.info("access token:{}", tokens.getAccessToken());
         log.info("refresh token:{}", tokens.getRefreshToken());
 
@@ -50,30 +44,4 @@ public class CreateUserServiceImpl implements CreateUserService {
                 .build();
     }
 
-    private JwtToken createJwtToken(User user) {
-        // JWT 토큰 생성을 위한 claims 생성
-        HashMap<String, String> claims = new HashMap<>();
-        claims.put(ROLE_CLAIM, user.getUserRole().name());
-        claims.put(NAME_CLAIM, user.getName());
-        claims.put(PROFILE_IMAGE_CLAIM, user.getProfileImageUrl());
-
-        // Access Token 생성
-        final String accessToken = jwtService.generateAccessToken(claims, user);
-        // Refresh Token 생성
-        final String refreshToken = jwtService.generateRefreshToken(claims, user);
-
-        log.info(">>>> {} generate Tokens", user.getName());
-
-        // Refresh Token 저장 - REDIS
-//        RefreshToken rt = RefreshToken.builder()
-//                .refreshToken(refreshToken)
-//                .email(user.getEmail())
-//                .build();
-//        refreshTokenService.saveRefreshToken(rt);
-
-        return JwtToken.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
 }

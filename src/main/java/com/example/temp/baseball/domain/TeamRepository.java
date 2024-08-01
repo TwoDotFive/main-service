2116d8f804a475a2eb182b1b95e6d59958fe7aaf
seonmi park <sm000117@naver.com>
2024-08-01 10:42:32 +0900

    feat: 시즌에 해당하는 팀 목록 조회 기능

--- src/main/java/com/example/temp/baseball/domain/TeamRepository.java
+++ src/main/java/com/example/temp/baseball/domain/TeamRepository.java
@@ -4,13 +4,17 @@ import com.example.temp.common.exception.CustomException;
 import org.springframework.data.repository.Repository;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends Repository<Team, Long> {
     Optional<Team> findById(long id);
 
+    List<Team> findBySeason(Season season);
+
     default Team findByIdOrElseThrow(long id) {
         return findById(id)
                 .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, "Team not found"));
     }
+
 }

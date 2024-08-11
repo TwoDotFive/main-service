99a231bfe1fe1b8e661baf2df11a96f7e126c931
csct3434 <csct3434@gmail.com>
2024-08-11 17:36:40 +0900

    test: 국문 관광정보 조회 API 매핑 테스트 오류 정정 및 가독성 개선
    - 기존 테스트 코드의 response.get~() 부분이 동작하지 않아 이를 정정했습니다.
    
    - 테스트 코드의 가독성을 개선했습니다.

--- src/test/java/com/example/temp/tour/dto/TourInformationBasedLocationResponseTest.java
+++ src/test/java/com/example/temp/tour/dto/TourInformationBasedLocationResponseTest.java
@@ -1,103 +1,107 @@
 package com.example.temp.tour.dto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+@DisplayName("국문 관광정보 조회 API")
+@ExtendWith(SoftAssertionsExtension.class)
 class TourInformationBasedLocationResponseTest {
-    private TourInformationBasedLocationResponse response;
+
+    private FindTourInformationByLocationHttpResponse response;
+
+    @InjectSoftAssertions
+    SoftAssertions softAssertions;
 
     @BeforeEach
     public void setUp() throws Exception {
-        String json = "{\n" +
-                "  \"response\": {\n" +
-                "    \"header\": {\n" +
-                "      \"resultCode\": \"0000\",\n" +
-                "      \"resultMsg\": \"OK\"\n" +
-                "    },\n" +
-                "    \"body\": {\n" +
-                "      \"items\": {\n" +
-                "        \"item\": [\n" +
-                "          {\n" +
-                "            \"addr1\": \"서울특별시 중구 을지로9길 6\",\n" +
-                "            \"addr2\": \"\",\n" +
-                "            \"areacode\": \"1\",\n" +
-                "            \"cat1\": \"A05\",\n" +
-                "            \"cat2\": \"A0502\",\n" +
-                "            \"cat3\": \"A05020400\",\n" +
-                "            \"contentid\": \"2685273\",\n" +
-                "            \"contenttypeid\": \"39\",\n" +
-                "            \"createdtime\": \"20201124213059\",\n" +
-                "            \"dist\": \"651.403118549015\",\n" +
-                "            \"firstimage\": \"http://tong.visitkorea.or.kr/cms/resource/42/2685142_image2_1.jpg\",\n" +
-                "            \"mapx\": \"126.9885339952\",\n" +
-                "            \"mapy\": \"37.5665544247\",\n" +
-                "            \"modifiedtime\": \"20210315185719\",\n" +
-                "            \"sigungucode\": \"24\",\n" +
-                "            \"tel\": \"02-2267-3939\",\n" +
-                "            \"title\": \"가야성\"\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      },\n" +
-                "      \"numOfRows\": 10,\n" +
-                "      \"pageNo\": 1,\n" +
-                "      \"totalCount\": 468\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+        String json = """
+                {
+                  "response": {
+                    "header": {
+                      "resultCode": "0000",
+                      "resultMsg": "OK"
+                    },
+                    "body": {
+                      "items": {
+                        "item": [
+                          {
+                            "addr1": "서울특별시 중구 을지로9길 6",
+                            "addr2": "",
+                            "areacode": "1",
+                            "cat1": "A05",
+                            "cat2": "A0502",
+                            "cat3": "A05020400",
+                            "contentid": "2685273",
+                            "contenttypeid": "39",
+                            "createdtime": "20201124213059",
+                            "dist": "651.403118549015",
+                            "firstimage": "http://tong.visitkorea.or.kr/cms/resource/42/2685142_image2_1.jpg",
+                            "mapx": "126.9885339952",
+                            "mapy": "37.5665544247",
+                            "modifiedtime": "20210315185719",
+                            "sigungucode": "24",
+                            "tel": "02-2267-3939",
+                            "title": "가야성"
+                          }
+                        ]
+                      },
+                      "numOfRows": 10,
+                      "pageNo": 1,
+                      "totalCount": 468
+                    }
+                  }
+                }""";
 
         ObjectMapper objectMapper = new ObjectMapper();
-        response = objectMapper.readValue(json, TourInformationBasedLocationResponse.class);
+        response = objectMapper.readValue(json, FindTourInformationByLocationHttpResponse.class);
     }
 
     @Test
-    void testGetAreacode() {
-        String areacode = response.getAreacode(0);
-        assertNotNull(areacode);
-        assertEquals("1", areacode);
-    }
+    @DisplayName("HTTP 응답 객체 매핑 테스트")
+    void test() {
+        List<FindTourInformationByLocationHttpResponse.Response.Body.Items.Item> foundItems = response.getItems();
 
-    @Test
-    void testGetSigungucode() {
-        String sigungucode = response.getSigungucode(0);
-        assertNotNull(sigungucode);
-        assertEquals("24", sigungucode);
-    }
+        // 조회 결과
+        softAssertions.assertThat(foundItems.size())
+                .as("조회된 관광지의 개수")
+                .isEqualTo(1);
 
-    @Test
-    void testGetTitle() {
-        String title = response.getTitle(0);
-        assertNotNull(title);
-        assertEquals("가야성", title);
-    }
+        FindTourInformationByLocationHttpResponse.Response.Body.Items.Item foundItem = foundItems.get(0);
 
-    @Test
-    void testGetAddr1() {
-        String addr1 = response.getAddr1(0);
-        assertNotNull(addr1);
-        assertEquals("서울특별시 중구 을지로9길 6", addr1);
-    }
+        softAssertions.assertThat(foundItem.getAreacode())
+                .as("지역 코드")
+                .isEqualTo("1");
 
-    @Test
-    void testGetTel() {
-        String tel = response.getTel(0);
-        assertNotNull(tel);
-        assertEquals("02-2267-3939", tel);
-    }
+        softAssertions.assertThat(foundItem.getSigungucode())
+                .as("시군구 코드")
+                .isEqualTo("24");
 
-    @Test
-    void testGetDist() {
-        String dist = response.getDist(0);
-        assertNotNull(dist);
-        assertEquals("651.403118549015", dist);
-    }
+        softAssertions.assertThat(foundItem.getTitle())
+                .as("제목")
+                .isEqualTo("가야성");
 
-    @Test
-    void testGetRepresentativeImageUrl() {
-        String representativeImageUrl = response.getRepresentativeImageUrl(0);
-        assertNotNull(representativeImageUrl);
-        assertEquals("http://tong.visitkorea.or.kr/cms/resource/42/2685142_image2_1.jpg", representativeImageUrl);
+        softAssertions.assertThat(foundItem.getAddr1())
+                .as("주소")
+                .isEqualTo("서울특별시 중구 을지로9길 6");
+
+        softAssertions.assertThat(foundItem.getTel())
+                .as("연락처")
+                .isEqualTo("02-2267-3939");
+
+        softAssertions.assertThat(foundItem.getDist())
+                .as("거리")
+                .isEqualTo("651.403118549015");
+
+        softAssertions.assertThat(foundItem.getRepresentativeImageUrl())
+                .as("대표 이미지 주소")
+                .isEqualTo("http://tong.visitkorea.or.kr/cms/resource/42/2685142_image2_1.jpg");
     }
 }

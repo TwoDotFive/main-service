020ee8e999e0fded2f41ee6cb9b35ec676f3fc62
csct3434 <csct3434@gmail.com>
2024-08-27 13:01:06 +0900

    feat: 투어 로그 메모 이미지 Soft Delete 기능 제거
    - 추후 인프라 작업 시 Soft Delete 재검토 예정

--- src/main/java/com/example/temp/tour/domain/TourScheduleMemoImage.java
+++ src/main/java/com/example/temp/tour/domain/TourScheduleMemoImage.java
@@ -6,15 +6,11 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Table(name = "tour_schedule_memo_image")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLRestriction("deleted = false")
-@SQLDelete(sql = "UPDATE tour_schedule_memo_image SET memo_id = null, deleted = true WHERE id = ?")
 public class TourScheduleMemoImage extends BaseTimeEntity {
 
     @Id
@@ -30,13 +26,10 @@ public class TourScheduleMemoImage extends BaseTimeEntity {
     @Column(nullable = false)
     private Integer sequence;
 
-    private Boolean deleted;
-
     public TourScheduleMemoImage(TourScheduleMemo memo, String imageUrl, Integer sequence) {
         this.id = IdUtil.create();
         this.memo = memo;
         this.imageUrl = imageUrl;
         this.sequence = sequence;
-        this.deleted = false;
     }
 }

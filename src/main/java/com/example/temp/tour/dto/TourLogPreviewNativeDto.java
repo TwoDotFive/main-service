6cf43ea75ff4615a50c611f69635efecfb90f1ab
csct3434 <csct3434@gmail.com>
2024-09-03 15:27:56 +0900

    feat: 투어 로그 최신순 조회 API 구현

---
+++ src/main/java/com/example/temp/tour/dto/TourLogPreviewNativeDto.java
@@ -0,0 +1,16 @@
+package com.example.temp.tour.dto;
+
+public interface TourLogPreviewNativeDto {
+
+    String getId();
+
+    String getImage();
+
+    String getTitle();
+
+    String getUserNickname();
+
+    String getUserProfileImage();
+
+    String getStadiumName();
+}

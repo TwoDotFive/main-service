b479492ae5764c044b6a6613100ba5845c22a6e3
csct3434 <csct3434@gmail.com>
2024-09-03 14:19:27 +0900

    refactor: 미사용 필드 제거

--- src/main/java/com/example/temp/common/config/JpaConfig.java
+++ src/main/java/com/example/temp/common/config/JpaConfig.java
@@ -2,7 +2,6 @@ package com.example.temp.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,9 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {

f77d10698568352402afc1fb2ac9e1b27ac5c9ed
Seonmi <sm000117@naver.com>
2024-09-29 11:16:04 +0900

    fix: Fanpool 서비스에 @Transactional(readonly) 추가

--- src/main/java/com/example/temp/fanpool/service/impl/FindFanpoolByIdServiceImpl.java
+++ src/main/java/com/example/temp/fanpool/service/impl/FindFanpoolByIdServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.temp.fanpool.dto.FindFanpoolBasedLocationResponse;
 import com.example.temp.fanpool.service.FindFanpoolByIdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +14,7 @@ public class FindFanpoolByIdServiceImpl implements FindFanpoolByIdService {
     private final FanpoolRepository fanpoolRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public FindFanpoolBasedLocationResponse doService(long id) {
         Fanpool found = fanpoolRepository.findByIdOrElseThrow(id);
         return FindFanpoolBasedLocationResponse.build(found);
